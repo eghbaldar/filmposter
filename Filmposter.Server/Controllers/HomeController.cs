e2e002c94960d937c6bb4a02caba2cfe7f30cc3c
eghbaldar <ali.eghbaldar2@gmail.com>
2025-03-29 11:21:21 +0330

    added Server projecet and connected them together

---
+++ Filmposter.Server/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Filmposter.Server.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok(); 
+        }
+    }
+}
--- Filmposter/Program.cs
+++ Filmposter/Program.cs
@@ -1,23 +1,38 @@
-using Filmposter.Components;
+﻿using Filmposter.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+
+builder.Services.AddHttpClient();
+
+// set API settings
+var environment = builder.Environment.EnvironmentName;
+string apiBaseUrl;
+if (environment == "Development")
+{
+    apiBaseUrl = builder.Configuration["ApiBaseUrl_Localhost"]; // Local API URL
+}
+else if (environment == "Production")
+{
+    apiBaseUrl = builder.Configuration["ApiBaseUrl_Server"]; // Production (server) API URL
+}
+else
+{
+    throw new Exception("Unknown environment: " + environment);
+}
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiBaseUrl) });
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
 app.UseAntiforgery();
 
